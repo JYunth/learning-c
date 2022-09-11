cd60dbba3d6e6459e58ca3c52ceb76f9f61980e3
JYunth <jyunth28@gmail.com>
2022-09-11 16:56:25 +0530

    Modified ascii.c

--- ascii.c
+++ ascii.c
@@ -4,11 +4,13 @@
 //If you want to store a character then you should store it in a variable of the char type.
 //But you can display that char as a number also, using the format placeholder. 
 
-int main(){
+int main()
+{
     char letter;
     printf("Enter a letter for which to display ASCII: ");
     scanf("%c", &letter);
 
     printf("The ASCII value of the letter you entered is: %d\n", letter);
+    printf("Thank you fro using my program!\n");
     return 0;
 }
